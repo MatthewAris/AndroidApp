fd351d8e44bca7512ab049ad8bd6f2b000e89219
Matthew Aris <124069912+MatthewAris@users.noreply.github.com>
2023-10-23 11:54:43 +0100

    Updated Gesture Testing

--- AppNumber1/app/src/main/java/uk/ac/tees/mgd/b1032293/weekoneapp/MainActivity.java
+++ AppNumber1/app/src/main/java/uk/ac/tees/mgd/b1032293/weekoneapp/MainActivity.java
@@ -1,10 +1,12 @@
 package uk.ac.tees.mgd.b1032293.weekoneapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,16 +22,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = findViewById(R.id.editTextText);
+        //editText = findViewById(R.id.txt_GameTitle);
     }
 
     public void onTouch(View v) {
         Log.d("MainActivity", "Button Pressed");
         Intent intent = new Intent(this, MainActivity2.class);
-        String s = editText.getText().toString();
+        /*String s = editText.getText().toString();
         if (s != null) {
             intent.putExtra(key, s);
-        }
+        }*/
         startActivity(intent);
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_UP:
+                Intent intent = new Intent(this, MainActivity2.class);
+                startActivity(intent);
+                break;
+            //case MotionEvent.AX
+        }
+        return true;
+    }
+
 }
--- AppNumber1/app/src/main/java/uk/ac/tees/mgd/b1032293/weekoneapp/MainActivity2.java
+++ AppNumber1/app/src/main/java/uk/ac/tees/mgd/b1032293/weekoneapp/MainActivity2.java
@@ -1,19 +1,69 @@
 package uk.ac.tees.mgd.b1032293.weekoneapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    TextView posXView, posYView, winScreen;
+    MainActivity mainActivity;
+    ImageView boatShape;
+    ImageView targetShape;
+    int movementInteger = 50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        // TODO:
-        // Bind TextView and the button
-        // extract information from SavedInstanceState
-        // Set the textView to the content (extra)
-        // Setup the button to let us return to MainActivity
+
+        posXView = findViewById(R.id.textX);
+        posYView = findViewById(R.id.textY);
+        winScreen = findViewById(R.id.winText);
+
+        boatShape = findViewById(R.id.boatShape);
+        targetShape = findViewById(R.id.goalShape);
     }
+
+    public void onTouchBack(View v){
+        Log.d("MainActivity2", "Button Pressed");
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+        finish();
+    }
+    public boolean onTouchEvent(MotionEvent event) {
+        posXView.setText("X: " + targetShape.getX() + "Y: " + targetShape.getY());
+        posYView.setText("X: " + boatShape.getX() + "Y: " + boatShape.getY());
+
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_UP:
+                boatShape.setY(boatShape.getY() - movementInteger);
+                if (checkOverlap(boatShape)){ winScreen.setVisibility(View.VISIBLE); }
+                break;
+        }
+        return true;
+    }
+    public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY) {
+        //Log.d(DEBUG_TAG, "onFling: " + event1.toString() + event2.toString());
+        return true;
+    }
+    public boolean checkOverlap(ImageView checking) {
+        if (checking.getX() >= targetShape.getX() - 5 && checking.getX() <= targetShape.getX() + 5){
+            if (checking.getY() >= targetShape.getY() - 5 && checking.getY() <= targetShape.getY() + 5){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
