0948bf17efb9a4db57469090088e21fcf238e9db
Matthew Aris <124069912+MatthewAris@users.noreply.github.com>
2023-10-30 11:48:30 +0000

    GestureDetectorImplementation

--- AppNumber1/app/src/main/java/uk/ac/tees/mgd/b1032293/weekoneapp/MainActivity2.java
+++ AppNumber1/app/src/main/java/uk/ac/tees/mgd/b1032293/weekoneapp/MainActivity2.java
@@ -1,31 +1,37 @@
 package uk.ac.tees.mgd.b1032293.weekoneapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.core.view.GestureDetectorCompat;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
     TextView posXView, posYView, winScreen;
     MainActivity mainActivity;
-    ImageView boatShape;
-    ImageView targetShape;
+    ImageView boatShape, targetShape;
     int movementInteger = 50;
+
+    GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        gestureDetector = new GestureDetector(this, this);
 
         posXView = findViewById(R.id.textX);
         posYView = findViewById(R.id.textY);
@@ -35,13 +41,7 @@ public class MainActivity2 extends AppCompatActivity {
         targetShape = findViewById(R.id.goalShape);
     }
 
-    public void onTouchBack(View v){
-        Log.d("MainActivity2", "Button Pressed");
-//        Intent intent = new Intent(this, MainActivity.class);
-//        startActivity(intent);
-        finish();
-    }
-    public boolean onTouchEvent(MotionEvent event) {
+   /* public boolean onTouchEvent(MotionEvent event) {
         posXView.setText("X: " + targetShape.getX() + "Y: " + targetShape.getY());
         posYView.setText("X: " + boatShape.getX() + "Y: " + boatShape.getY());
 
@@ -52,11 +52,13 @@ public class MainActivity2 extends AppCompatActivity {
                 break;
         }
         return true;
+    }*/
+
+    public void onTouchBack(View v){
+        Log.d("MainActivity2", "Button Pressed");
+        finish();
     }
-    public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY) {
-        //Log.d(DEBUG_TAG, "onFling: " + event1.toString() + event2.toString());
-        return true;
-    }
+
     public boolean checkOverlap(ImageView checking) {
         if (checking.getX() >= targetShape.getX() - 5 && checking.getX() <= targetShape.getX() + 5){
             if (checking.getY() >= targetShape.getY() - 5 && checking.getY() <= targetShape.getY() + 5){
@@ -64,6 +66,88 @@ public class MainActivity2 extends AppCompatActivity {
             }
         }
         return false;
+    }
+
+    @Override
+    public boolean onDown(@NonNull MotionEvent motionEvent) {
+        return true;
+    }
+    @Override
+    public void onShowPress(@NonNull MotionEvent motionEvent) {
+    }
+    @Override
+    public boolean onSingleTapUp(@NonNull MotionEvent motionEvent) {
+        return false;
+    }
+    @Override
+    public boolean onScroll(@NonNull MotionEvent motionEvent, @NonNull MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+    @Override
+    public void onLongPress(@NonNull MotionEvent motionEvent) {
+    }
+
+    private static final int SWIPE_THRESHOLD = 10;
+    private static final int SWIPE_VELOCITY_THRESHOLD = 10;
+
+    @Override
+    public boolean onFling(@NonNull MotionEvent e1, @NonNull MotionEvent e2, float velocityX, float velocityY) {
+
+        posXView.setText("X: " + targetShape.getX() + "Y: " + targetShape.getY());
+        posYView.setText("X: " + boatShape.getX() + "Y: " + boatShape.getY());
+
+
+        float diffY = e2.getY() - e1.getY();
+        float diffX = e2.getX() - e1.getX();
+
+        if (Math.abs(diffX) > Math.abs(diffY)) {
+            // Horizontal Swipe
+            if ((Math.abs(diffX) > SWIPE_THRESHOLD) && (Math.abs(velocityX)) > SWIPE_VELOCITY_THRESHOLD) {
+                if (diffX > 0) {
+                    onSwipeRight();
+                } else {
+                    onSwipeLeft();
+                }
+            }
+        } else {
+        // Vertical Swipe
+            if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                if (diffY > 0) {
+                    onSwipeDown();
+                } else {
+                    onSwipeUp();
+                }
+            }
+        }
+        return true;
+    }
+
+    private void onSwipeUp() {
+        boatShape.setY(boatShape.getY() - movementInteger);
+        if (checkOverlap(boatShape)) {
+            winScreen.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void onSwipeDown() {
+        boatShape.setY(boatShape.getY() + movementInteger);
+        if (checkOverlap(boatShape)) {
+            winScreen.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void onSwipeLeft() {
+        boatShape.setX(boatShape.getX() - movementInteger);
+        if (checkOverlap(boatShape)) {
+            winScreen.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void onSwipeRight() {
+        boatShape.setX(boatShape.getX() + movementInteger);
+        if (checkOverlap(boatShape)) {
+            winScreen.setVisibility(View.VISIBLE);
+        }
     }
 
 }
--- AppNumber1/app/src/main/java/uk/ac/tees/mgd/b1032293/weekoneapp/OnSwipeTouchListener.java
+++ AppNumber1/app/src/main/java/uk/ac/tees/mgd/b1032293/weekoneapp/OnSwipeTouchListener.java
@@ -9,6 +9,8 @@ import androidx.annotation.NonNull;
 
 public class OnSwipeTouchListener implements View.OnTouchListener {
 
+    private static final int SWIPE_THRESHOLD = 100;
+    private static final int SWIPE_VELOCITY_THRESHOLD = 100;
     private final GestureDetector gestureDetector;
     public OnSwipeTouchListener (Context ctx) {
         gestureDetector = new GestureDetector(ctx,
@@ -26,7 +28,7 @@ public class OnSwipeTouchListener implements View.OnTouchListener {
             float diffX = e2.getX() - e1.getX();
             if (Math.abs(diffX) > Math.abs(diffY)){
                 // TODO: ADD SWIPE VARIABLES AND FUNCTIONS
-                if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX)) > SWIPE_VELOCITY_THRESHOLD) {
+                if ((Math.abs(diffX) > SWIPE_THRESHOLD) && (Math.abs(velocityX)) > SWIPE_VELOCITY_THRESHOLD) {
                     if (diffX > 0) {
                         onSwipeRight();
                     } else {
@@ -35,7 +37,7 @@ public class OnSwipeTouchListener implements View.OnTouchListener {
                 }
                 result = true;
             }
-            else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(VelocityY) > SWIPE_VELOCITY_THRESHOLD) {
+            else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
                 if (diffX > 0){
                     onSwipeBottom();
                 } else {
@@ -50,32 +52,37 @@ public class OnSwipeTouchListener implements View.OnTouchListener {
 
 }
 
+    private void onSwipeTop() {
+    }
+
+    private void onSwipeBottom() {
+    }
+
+    private void onSwipeLeft() {
+    }
+
+    private void onSwipeRight() {
+    }
+
     private class GestureListener implements GestureDetector.OnGestureListener {
         @Override
         public boolean onDown(@NonNull MotionEvent motionEvent) {
             return false;
         }
-
         @Override
         public void onShowPress(@NonNull MotionEvent motionEvent) {
-
         }
-
         @Override
         public boolean onSingleTapUp(@NonNull MotionEvent motionEvent) {
             return false;
         }
-
         @Override
         public boolean onScroll(@NonNull MotionEvent motionEvent, @NonNull MotionEvent motionEvent1, float v, float v1) {
             return false;
         }
-
         @Override
         public void onLongPress(@NonNull MotionEvent motionEvent) {
-
         }
-
         @Override
         public boolean onFling(@NonNull MotionEvent motionEvent, @NonNull MotionEvent motionEvent1, float v, float v1) {
             return false;
